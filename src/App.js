61d3d4a03bda5bb1ea717c199ebbbc566ddfe358
Arsalan Ali Khattak <thunder9905@gmail.com>
2022-10-03 23:12:36 +0500

    cards working perfectly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
+import Card from "./Components/Card"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Card/>
     </div>
   );
 }
